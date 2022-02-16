652ff3d4ea804aa8a8fa86b6a2ab0f500968c589
j0rdan0 <georgepaunesco@gmail.com>
2022-02-16 18:29:52 +0200

    first usefull function in cpp(sort of c though)

--- testing1/test1.cpp
+++ testing1/test1.cpp
@@ -1,6 +1,24 @@
 #include <stdio>
+#include <windows.h>
+
+void fperror(DWORD);
+
+using namespace std;
 
 int main() {
 	cout << "debug";
 	return 0;
-}\
+}
+void fperror(DWORD errCode) {
+	LPVOID msg = NULL;
+	DWORD systemLocale = MAKELANGID(LANG_NEUTRAL, SUBLANG_NEUTRAL);
+
+	if (FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_ALLOCATE_BUFFER, NULL, errCode, systemLocale, (PTSTR)&msg, 0, NULL)) {
+		if (!msg) {
+			printf("[-] error string not available\n");
+		}
+		else {
+			wprintf(L"[-] %ls", (LPWSTR)msg);
+		}
+	}
+}
